6aab2fc14968afa1a6e25975d05cfdfd3dd6fbce
KTorres <kar33en@gmail.com>
2025-05-20 17:47:35 -0400

    resolviendo

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function printGreeting(greeting) {
+    console.log(greeting);
+}
+ printGreeting("Hello");
+
+ function greetTheUser(firstName, lastName) {
+  console.log ("Hello," + " " + firstName + " " + lastName);
+
+ }
+ greetTheUser ("Pepita", " Perez");
